8cf9b41434f46191dd228c1341f286c457c1a34f
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2025-02-05 14:38:52 +0530

    Attendance Checked of Employee Present or Abssent using RANDOM

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@ namespace Employee_Wage_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage");
-        }
-       
-        
-    }
 
+            Random random = new Random();
+
+            int attendanceCheck = random.Next(0, 2);
+
+            if (attendanceCheck == 1)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
+        }
+
+    }
 }
