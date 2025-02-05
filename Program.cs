ee9c0c33264e34de7306e445e6e2ecb1832ea82e
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2025-02-05 14:32:55 +0530

    Welcome to Employee Wage Problem

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee_Wage_Problem
+{
+    class Employee
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage");
+        }
+       
+        
+    }
+
+}
