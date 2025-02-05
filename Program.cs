ddfc2e68f13afd9e59b4ea42669c9936a26d100b
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2025-02-05 15:19:01 +0530

    Calculated Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ namespace Employee_Wage_Problem
 {
     class Employee
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage");
+        const int Wage_per_Hour = 20;
+        const int Full_Time_Working_Hours = 8;
 
+        public void Employee_Attendance()
+        {
             Random random = new Random();
 
             int attendanceCheck = random.Next(0, 2);
@@ -20,6 +21,20 @@ namespace Employee_Wage_Problem
             {
                 Console.WriteLine("Employee is Absent.");
             }
+        }
+        public void Employee_Wage()
+        {
+            int Full_Time_Wage = Wage_per_Hour*Full_Time_Working_Hours;
+            Console.WriteLine("Daily Employee Wage is: " + Full_Time_Wage+"Rs");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage");
+
+            Employee e = new Employee();
+            e.Employee_Attendance();
+            e.Employee_Wage();
         }
 
     }
